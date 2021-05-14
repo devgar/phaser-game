dbcac7420191aec00882fc69b939b7ab1fe1437a
Edgar Albalate <dev.gardo@gmail.com>
2021-05-14 17:51:50 +0200

    platform class property refactor

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -9,7 +9,6 @@ import Player from '../sprites/player'
 export default class GameScene extends Phaser.Scene {
   constructor () { super('GameScene') }
 
-  platforms?: Phaser.Physics.Arcade.StaticGroup
   player?: Player
 
   preload() {
@@ -23,15 +22,15 @@ export default class GameScene extends Phaser.Scene {
   create() {
     this.add.image(400, 300, 'sky')
 
-    this.platforms = this.physics.add.staticGroup()
-    this.platforms.create(400, 410, 'ground').setScale(2).refreshBody()
-    this.platforms.create(600, 350, 'ground')
-    this.platforms.create(50, 250, 'ground')
-    this.platforms.create(750, 220, 'ground')
+    const platforms = this.physics.add.staticGroup()
+    platforms.create(400, 418, 'ground').setScale(2).refreshBody()
+    platforms.create(600, 350, 'ground')
+    platforms.create(50, 250, 'ground')
+    platforms.create(750, 220, 'ground')
 
     this.player = new Player(this, 220, 200)
 
-    this.physics.add.collider(this.player, this.platforms)
+    this.physics.add.collider(this.player, platforms)
   }
 
   update() {
