2406ed6ca4eb2c22e2fa35c3e70d8dfc37062de8
Edgar Albalate <dev.gardo@gmail.com>
2021-05-19 19:18:51 +0200

    Added types aliaser file

---
+++ src/types.ts
@@ -0,0 +1,5 @@
+import Phaser from 'phaser'
+
+export type Animation = Phaser.Types.Animations.Animation
+export const ArcadeSprite = Phaser.Physics.Arcade.Sprite
+export type ArcadeSpriteType = Phaser.Physics.Arcade.Sprite
