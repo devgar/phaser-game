009ebce689446021e60c69ec3087d87915e198fa
Edgar Albalate <dev.gardo@gmail.com>
2021-05-19 19:18:38 +0200

    Added assets import file

---
+++ src/assets.ts
@@ -0,0 +1,4 @@
+export { default as sky } from './assets/sky.png'
+export { default as platform } from './assets/platform.png'
+export { default as star } from './assets/star.png'
+export { default as bomb } from './assets/bomb.png'
